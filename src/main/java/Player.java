5cfe4057edbca00933c57b8ccad86506b1616b2e
magnusforbord <magnus_ff@hotmail.com>
2022-03-24 11:32:16 +0100

    Created the Player.java class

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -10,11 +10,13 @@ public class Player {
     }
 
     public String getName() {
-        return name;
+        return firstName + " " + lastName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String firstName, String lastName) {
+        if (firstName.isBlank()||lastName.isBlank()){throw new IllegalArgumentException("The player needs a full name!");}
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     public int getAge() {
@@ -22,14 +24,16 @@ public class Player {
     }
 
     public void setAge(int age) {
-        this.age = age;
+        if (age<0){throw new IllegalArgumentException("The player cannot be lower than 0.");}
+        this.age=age;
     }
 
-    public String getGender() {
+    public String getGender(){
         return gender;
     }
 
     public void setGender(String gender) {
+        if (gender.isBlank()){throw new IllegalArgumentException("The gender must be spesified.");}
         this.gender = gender;
     }
 }
