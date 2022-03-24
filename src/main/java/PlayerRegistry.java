11df37abace584e451b196782385f50130750a88
magnusforbord <magnus_ff@hotmail.com>
2022-03-24 10:13:43 +0100

    Created the PlayerRegistry.java class.

---
+++ src/main/java/PlayerRegistry.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class PlayerRegistry {
+
+    public ArrayList<Player> players = new ArrayList<>();
+
+    public ArrayList<Player> getPlayers(){
+        return players;
+    }
+
+    public Player newPlayer(String firstName, String lastName, int age, String gender){
+        Player newPlayer = new Player(firstName, lastName, age, gender);
+        players.add(newPlayer);
+        return newPlayer;
+    }
+    
+    // Method for finding a player by name, if necessary
+    
+    public ArrayList<Player> getPlayerByName(String firstName, String lastName){
+        ArrayList<Player> playerName = new ArrayList<>();
+        for (int i = 0; i < getPlayers().size(); i++){
+            if(getPlayers().get(i).getName().equals(playerName)){
+                playerName.add(getPlayers().get(i));
+            }
+        }
+        return playerName;
+    }
+    
+}
