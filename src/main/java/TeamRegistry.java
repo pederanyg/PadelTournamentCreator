b8230ae2e08f3071fc9e03a58ca862fcc34f8817
magnusforbord <magnus_ff@hotmail.com>
2022-03-24 10:13:19 +0100

    Created the TeamRegistry.java class.

---
+++ src/main/java/TeamRegistry.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class TeamRegistry {
+
+    public ArrayList<Team> teams = new ArrayList<>();
+
+    public ArrayList<Team> getTeams() {
+        return teams;
+    }
+
+    public Team addNewTeam(String teamName, Player player1, Player player2){
+        Team newTeam = new Team(teamName, player1, player2);
+        teams.add(newTeam);
+        return newTeam;
+    }
+
+
+}
