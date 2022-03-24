4c5ae1bf59172a1929948d79abbac67feb18bdfa
magnusforbord <magnus_ff@hotmail.com>
2022-03-24 10:14:04 +0100

    Created the SinglesMatch.java class.

---
+++ src/main/java/SinglesMatch.java
@@ -0,0 +1,24 @@
+public class SinglesMatch {
+
+    private Player playerOne;
+    private Player playerTwo;
+    private Player winner;
+
+    public SinglesMatch(Player playerOne, Player playerTwo){
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+    }
+
+    public void setSinglesResult(Player winner){
+        if (winner.equals(playerOne)){
+            this.winner = playerOne;
+        } else {
+            this.winner = playerTwo;}
+    }
+
+    @Override
+    public String toString() {
+        return "Match {" +
+                "Player One = " + playerOne + " | Player Two = " + playerTwo + "}";
+    }
+}
